6a4b10ae155a88fe30ad824ec52db9ddd7f03855
Pmea0202 <133823090+Pmea0202@users.noreply.github.com>
2024-02-02 15:49:51 -0500

    quitar librerias

--- vistas/tcpdf/tcpdf.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$pdf = new TCPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 14);
-$pdf->Cell(40, 10, '¡Hola, TCPDF!');
-
-$pdf->Output('example.pdf', 'I');
