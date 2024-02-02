69fd6bd173186a4982da2eb48ebc2e2b2f31b08e
NataliPostillon <157930524+NataliPostillon@users.noreply.github.com>
2024-02-02 11:32:27 -0500

    Update footer.php

--- vistas/footer.php
+++ vistas/footer.php
@@ -8,7 +8,7 @@
             <footer class="sticky-footer bg-white mt-5">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Website Estudio Contable C&A - 2022</span>
+                        <span>Copyright &copy; Website Estudio Contable C&A - 2024</span>
                     </div>
                 </div>
             </footer>
